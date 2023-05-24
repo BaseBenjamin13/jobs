ad5b8c1735a27f9037ff5d0128d22aa7d09af615
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-05-24 18:10:54 -0400

    setup welcome msg and fonts

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native';
+import { useRouter } from 'expo-router';
 
-import styles from './welcome.style'
+import styles from './welcome.style';
+import { icons, SIZES } from '../../../constants';
 
 const Welcome = () => {
-  return (
-    <View>
-      <Text>Welcome</Text>
-    </View>
-  )
+
+    const router = useRouter();
+
+    return (
+        <View>
+            <View style={styles.container}>
+                <Text style={styles.userName}>Hello Davidson</Text>
+                <Text style={styles.welcomeMessage}>Find your dream job!</Text>
+            </View>
+
+            <View style={styles.searchContainer}>
+                <View style={styles.searchWrapper}>
+                    <TextInput 
+                        style={styles.searchInput}
+                    />
+                </View>
+            </View>
+        </View>
+    )
 }
 
 export default Welcome
