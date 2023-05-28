3186c1c0d85d57b1fdf002d53a688bfbc3d005f9
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-05-27 23:50:19 -0400

    added header to jobDetails page

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,11 +1,48 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useCallback, useState } from 'react';
+import { Text, View, SafeAreaView, ScrollView, ActivityIndicator, RefreshControl }
+    from 'react-native';
+import { Stack, useRouter, useSearchParams } from 'expo-router';
 
+import { Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics }
+    from '../../components';
+import { COLORS, icons, SIZES } from '../../constants';
+import useFetch from '../../hook/useFetch';
 
 function JobDetails() {
-  return (
-    <Text>JobDetails</Text>
-  )
+    const params = useSearchParams();
+    const router = useRouter();
+
+    const { data, isLoading, error, refetch } = useFetch('job-details', {
+        job_id: params.id
+    });
+
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+            <Stack.Screen
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightWhite},
+                    headerShadowVisible: false,
+                    headerBackVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn
+                            iconUrl={icons.left}
+                            dimension="60%"
+                            handlePress={() => router.back()}
+                        />
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn
+                            iconUrl={icons.share}
+                            dimension="60%"
+                        />
+                    ),
+                    headerTitle: ''
+                }}
+            >
+
+            </Stack.Screen>
+        </SafeAreaView>
+    )
 }
 
 export default JobDetails;
