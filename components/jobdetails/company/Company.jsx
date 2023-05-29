09631ad81e4353639a26cc98c2f686a36afdc0ad
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-05-28 23:01:19 -0400

    added company section to job detail page

--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -18,6 +18,23 @@ const Company = ({ companyLogo, jobTitle, companyName, Location }) => {
                     style={styles.logoImage}
                 />
             </View>
+
+            <View style={styles.jobTitleBox}>
+                <Text style={styles.jobTitle}>{jobTitle}</Text>
+            </View>
+
+            <View style={styles.companyInfoBox}>
+                <Text style={styles.companyName}>{companyName} / </Text>
+                <View style={styles.locationBox}>
+                    <Image 
+                        source={icons.location}
+                        resizeMode='contain'
+                        style={styles.locationImage}
+                    />
+                    <Text style={styles.locationName}>{Location}</Text>
+                </View>
+            </View>
+
         </View>
     )
 }
